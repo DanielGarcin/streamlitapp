ebcafc2bf1760cb0163aa459caf64fe916860745
Daniel Garcin <dgarcin@gmail.com>
2026-02-14 14:39:55 +0100

    chargement du data set

--- customer_behaviour.py
+++ customer_behaviour.py
@@ -12,3 +12,17 @@ st.set_page_config(page_title="Analyse des comportements d'achat avec DuckDB", l
 # Titre de l'application
 st.title("Analyse des comportements d'achat avec DuckDB et Streamlit")
 st.write("Cette application analyse les données de comportement d'achat en utilisant DuckDB et Streamlit.")
+
+
+# Fonction pour charger les données de démonstration de comprtement d'achat
+def charger_donnees_comportement_achat():
+    # URL des données de comportement d'achat
+    url = "https://www.kaggle.com/datasets/ayeshasiddiqa123/customer-shopping-behavior-dataset"
+    return pd.read_csv(url)
+
+# Sidebar pour le chargement des données
+st.sidebar.title("Source de données")
+source_option = st.sidebar.radio(
+    "Choisir la source de données:",
+    ["Données Titanic de démonstration", "Télécharger un fichier CSV"]
+)
